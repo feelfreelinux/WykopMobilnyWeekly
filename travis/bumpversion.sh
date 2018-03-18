5e6b694cd500b1f8a3038d61b0720d2b21647ed6
Filip <rj23@protonmail.com>
2018-03-18 22:58:24 +0100

    Trigger build

--- travis/bumpversion.sh
+++ travis/bumpversion.sh
@@ -3,6 +3,9 @@
 set -e
 
 currentVersionCode=$(grep 'versionBuild = ' WykopMobilny/build.gradle | sed -r 's/versionBuild = //g;s/ //g');
+versionMajor=$(grep 'versionMajor = ' WykopMobilny/build.gradle | sed -r 's/versionMajor = //g;s/ //g');
+versionMinor=$(grep 'versionMinor = ' WykopMobilny/build.gradle | sed -r 's/versionMinor = //g;s/ //g');
+versionPatch=$(grep 'versionPatch = ' WykopMobilny/build.gradle | sed -r 's/versionPatch = //g;s/ //g');
 
 newVersionCode=$((currentVersionCode+1))
 
@@ -10,4 +13,4 @@ echo "New version code is ($newVersionCode) replacing"
 
 sed --in-place -r "s/versionBuild = ${currentVersionCode}/versionBuild = $newVersionCode/" WykopMobilny/build.gradle
 
-export OWMBUILDVERSION=$((newVersionCode))
+export OWMVERSION="$versionMajor.$versionMinor.$versionPatch.$newVersionCode"
