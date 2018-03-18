7a895c59d035f8ece88bc53fd5f791eb0e3d6042
Filip <rj23@protonmail.com>
2018-03-18 15:02:03 +0100

    Improved travis script

--- travis/bumpversion.sh
+++ travis/bumpversion.sh
@@ -2,10 +2,12 @@
 
 set -e
 
-currentVersionCode=$(grep 'versionBuild = ' build.gradle | sed -r 's/versionBuild = //g;s/ //g');
+currentVersionCode=$(grep 'versionBuild = ' WykopMobilny/build.gradle | sed -r 's/versionBuild = //g;s/ //g');
 
 newVersionCode=$((currentVersionCode+1))
 
 echo "New version code is ($newVersionCode) replacing"
 
-sed --in-place -r "s/versionBuild = ${currentVersionCode}/versionBuild = $newVersionCode/" build.gradle
+sed --in-place -r "s/versionBuild = ${currentVersionCode}/versionBuild = $newVersionCode/" WykopMobilny/build.gradle
+
+export OWMBUILDVERSION=$((newVersionCode))
